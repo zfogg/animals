48f261bf2f03b759c7b946a865d3e0db1633b4be
Zach Fogg <zach.fogg@gmail.com>
2014-01-21 02:27:34 -0500

    Annotations.

--- animals/animals.py
+++ animals/animals.py
@@ -3,17 +3,17 @@ import random
 from .types import types
 
 
-def randomSample(xs, n):
-    return random.sample(xs, n)
-
 def qAndA(type_, nmin, nmax):
     print("Choose number of {0} ({1} to {2}):".format(
         type_, nmin, nmax))
 
+    # Get a number (in base 10) from the user.
     n = int(raw_input(), 10)
+    # Ensure the number is between the min and the max.
     n = min(n, nmax)
     n = max(n, nmin)
 
-    print("{0}\n".format(randomSample(types[type_], n)))
+    sample = random.sample(types[type_], n)
 
+    print("{0}\n".format(sample))
 
